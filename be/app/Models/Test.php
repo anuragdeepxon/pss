b63316b2f65660e375cbfea815a8f9aa9b7b3a9d
Puneet Singla <Puneet Singla>
2022-12-13 11:04:18 +0530

    Initial Setup

---
+++ be/app/Models/Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    public $table = 'tests';
+
+    public $fillable = [
+        
+    ];
+
+    protected $casts = [
+        
+    ];
+
+    public static $rules = [
+        
+    ];
+
+    
+}
